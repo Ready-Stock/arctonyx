a88eae08efb1f81a42f4b028700bdbc9c655e6c6
Elliot Courant <elliot.courant@readystock.io>
2018-10-11 21:12:59 -0500

    Adding more sequence functionality.
    I need to finish the Get/Set functionality first though.

--- sequence.go
+++ sequence.go
@@ -1,6 +1,7 @@
 package raft_badger
 
 import (
+	"context"
 	"github.com/hashicorp/raft"
 	"sync"
 )
@@ -16,18 +17,40 @@ func (store *Store) CreateNewSequence(name *string) (*uint64, error) {
 }
 
 type SequenceChunk struct {
-	response SequenceChunkResponse
-	sync     *sync.Mutex
+	current *SequenceChunkResponse
+	next    *SequenceChunkResponse
+	index   uint64
+	sync    *sync.Mutex
 }
 
-func (store *Store) NextSequenceValue(sequenceId uint64) (*uint64, error) {
+func (store *Store) NextSequenceValueById(sequenceId uint64) (*uint64, error) {
 	store.chunkMapMutex.Lock()
 	defer store.chunkMapMutex.Unlock()
+Reset:
 	if sequence, ok := store.sequenceChunks[sequenceId]; !ok {
 		if store.raft != nil && store.raft.State() != raft.Leader {
 			// Send request to leader.
+			if chunk, err := store.sequenceServiceC.GetSequenceChunkById(context.Background(), &SequenceChunkRequestById{SequenceId:sequenceId}); err != nil {
+				return nil, err
+			} else {
+				store.sequenceChunks[sequenceId] = &SequenceChunk{
+					current:chunk,
+					next:nil,
+					index:1,
+					sync:new(sync.Mutex),
+				}
+				goto Reset
+			}
 		} else {
 
 		}
+	} else {
+		return sequence.Next()
 	}
+}
+
+func (sequence *SequenceChunk) Next() (*uint64, error) {
+	sequence.sync.Lock()
+	defer sequence.sync.Unlock()
+
 }
